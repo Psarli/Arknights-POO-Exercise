4a8eb5d1324aaa67e0eec375eb46c4995d153011
Psarli <ppsarli2004@gmail.com>
2024-04-05 11:43:23 -0300

    atv yes

---
+++ Arknights PO exercise/Arknights PO exercise/Operators.swift
@@ -0,0 +1,26 @@
+//
+//  Operators.swift
+//  Arknights PO exercise
+//
+//  Created by Usuário Convidado on 05/04/24.
+//
+
+import Cocoa
+
+class Operators: NSObject {
+
+    var Classe: String
+    var AtkPower: Int
+    var RedeployTime : Float
+    var Infected : Bool
+    
+    override init() {
+        self.Classe=""
+        self.AtkPower = 0
+        self.RedeployTime = 0.0
+        self.Infected = false
+    }
+    
+    
+    
+}
---
+++ Arknights PO exercise/Arknights PO exercise/main.swift
@@ -0,0 +1,19 @@
+//
+//  main.swift
+//  Arknights PO exercise
+//
+//  Created by Usuário Convidado on 05/04/24.
+//
+
+import Foundation
+
+print("Hello, Doctor")
+
+var Pramanix = Operators()
+    Pramanix.Classe = "Debuff"
+    Pramanix.AtkPower = 342
+    Pramanix.RedeployTime = 65.0
+    Pramanix.Infected = false
+
+print("A operadora Pramanix sera designada para a classe \(Pramanix.Classe), devido a seu poder de diminuir RES e DEF inimiga, seu poder de fogo e de \(Pramanix.AtkPower), seu tempo de redeploy deve ser de \(Pramanix.RedeployTime)s, status de Infeccao: \(Pramanix.Infected).")
+
