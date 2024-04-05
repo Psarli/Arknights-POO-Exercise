5495a7331b1ce92417d40a18ad6280b9a6609617
Psarli <ppsarli2004@gmail.com>
2024-04-05 12:14:31 -0300

    func

--- Arknights PO exercise/Arknights PO exercise/Operators.swift
+++ Arknights PO exercise/Arknights PO exercise/Operators.swift
@@ -8,17 +8,34 @@
 import Cocoa
 
 class Operators: NSObject {
-
+    
     var Classe: String
     var AtkPower: Int
     var RedeployTime : Float
-    var Infected : Bool
+    var Promoted : Bool
+    var Infected: Bool
     
     override init() {
         self.Classe=""
         self.AtkPower = 0
         self.RedeployTime = 0.0
+        self.Promoted = false
         self.Infected = false
+    }
+    
+    func Promotion1(Promotion1: Bool)->Bool{
+        self.Promoted = true
+        return self.Promoted
+    }
+    
+    func PromotionPwr2(PromotionPwr2: Int)->Int{
+        self.AtkPower = 1400
+        return self.AtkPower
+    }
+    
+    func AltClass(AltClass: String)-> String{
+        self.Classe = "Debuff"
+        return self.Classe
     }
     
     
--- Arknights PO exercise/Arknights PO exercise/main.swift
+++ Arknights PO exercise/Arknights PO exercise/main.swift
@@ -10,10 +10,12 @@ import Foundation
 print("Hello, Doctor")
 
 var Pramanix = Operators()
-    Pramanix.Classe = "Debuff"
+    Pramanix.Classe = "Ranged"
     Pramanix.AtkPower = 342
     Pramanix.RedeployTime = 65.0
+    Pramanix.Promoted = true
     Pramanix.Infected = false
 
-print("A operadora Pramanix sera designada para a classe \(Pramanix.Classe), devido a seu poder de diminuir RES e DEF inimiga, seu poder de fogo e de \(Pramanix.AtkPower), seu tempo de redeploy deve ser de \(Pramanix.RedeployTime)s, status de Infeccao: \(Pramanix.Infected).")
+print("A operadora Pramanix sera designada para a classe \(Pramanix.Classe), com subclasse de \(AltClass) devido a seu poder de diminuir RES e DEF inimiga, seu poder de fogo e de \(Pramanix.AtkPower), seu tempo de redeploy deve ser de \(Pramanix.RedeployTime)s, status de Infeccao: \(Pramanix.Promoted). Promocao recebida: \(Pramanix.Promoted)")
+
 
